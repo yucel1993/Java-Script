93a3184d3d64c3a03f32b17ea073d3d3144ce142
Burhan <alpagut1993@gmail.com>
2023-06-05 12:16:04 +0200

    update

---
+++ GuessTheNumber/guess.js
@@ -0,0 +1,81 @@
+
+
+const input=document.querySelector("#input")
+const h2=document.querySelector(".h2")
+input.focus()
+const btn=document.querySelector("#btn")
+let random=Math.floor(Math.random()*100+1)
+
+let n=6
+
+
+input.addEventListener("keydown",(e)=>{
+    if(e.code=="Enter"){
+        btn.click()
+    }
+})
+
+btn.addEventListener("click",()=>{
+    
+
+    const input=document.querySelector("#input")
+    console.log(random)
+    if(input.value==random){
+        
+        h2.textContent = "Master mind";
+        
+        
+    }else if(input.value<random){
+        answer=`Increase your number.
+        You have ${n} chance left`
+        
+        h2.textContent = answer;
+        
+        n-- 
+    }else{
+        answer=`Decrease your number.
+        You have ${n} chance left`
+        
+        h2.textContent = answer;
+        n--
+    }
+    input.value=''
+    input.focus()
+
+})
+
+
+
+
+// const input=document.querySelector("#input")
+// const h2=document.querySelector(".h2")
+// const h3=document.querySelector(".h3")
+// const btn=document.querySelector("#btn")
+// let random=Math.floor(Math.random()*100+1)
+// btn.addEventListener("click",()=>{
+// console.log(input.value)
+// console.log(random)
+// let answer=''
+// let result=''
+// let n=2
+// while(n>0){
+//     // let user=+prompt("Enter your Number")
+//     console.log(random)
+//     if(input.value===random){
+//         result="MasterMind"
+//         console.log(result);
+//         h2.innerHTML=`${text}`
+//         break
+//     }else if(input.value<random){
+//        answer=`Increase your number.
+//        You have ${n} chance left`
+//        console.log(answer)
+//        n--
+//     }else{
+//         answer=`Decrease your number.
+//         You have ${n} chance left`
+//         console.log(answer)
+//         n--
+//     }
+// }
+// })
