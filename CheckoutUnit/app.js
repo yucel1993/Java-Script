ae743e7a73858fbe0ecbccc18483cf3eac3bfa3e
Burhan <alpagut1993@gmail.com>
2023-06-13 14:12:49 +0200

    2

--- CheckoutUnit/app.js
+++ CheckoutUnit/app.js
@@ -1,258 +1,81 @@
-// !Mac Info
+const vergiOrani = 0.18;
+const kargoÜcreti = 25.99;
 
-const MacbookPlusBtn=document.querySelector("#macPlus")
-const MacbookMinBtn=document.querySelector("#macMinus")
-const MacQuantity=document.querySelector("#quantityMac")
-const MacTotal=document.querySelector("#macTotal")
-const MacTrash=document.querySelector("#macTrash")
+// ?events
+const ürünlerPanelDivi = document.getElementById("product-painel");
+console.log(ürünlerPanelDivi);
 
-// !Keyboard info
-
-const keyboardPlusBtn=document.querySelector("#keyboardPlus")
-const keyboardMinBtn=document.querySelector("#keyboardMinus")
-const keyboardQuantity=document.querySelector("#quantityKeyboard")
-const keyboardTotal=document.querySelector("#keyboardTotal")
-const keyboardTrash=document.querySelector("#keyboardTrash")
-
-// !Apple info
-
-const ApplePlusBtn=document.querySelector("#applePlus")
-const AppleMinBtn=document.querySelector("#appleMinus")
-const AppleQuantity=document.querySelector("#quantityApple")
-const AppleTotal=document.querySelector("#appleTotal")
-const AppleTrash=document.querySelector("#appleTrash")
-
-//! Main section
-
-const allTrash=document.querySelector("#allTrash")
-const beforePrice=document.querySelector("#mainSum")
-const shippingPrice=document.querySelector("#cart-shipping")
-const shippingPriceNumber=document.querySelector("#shippingPriceNumber")
-
-const shippingPrice2 = parseFloat(shippingPriceNumber.textContent.replace(",", "."));
-
-const tax=document.querySelector("#tax")
-const total=document.querySelector("#total")
-
-const myChart=document.querySelector("allProduct")
-
-const body=document.querySelector("body")
-const allProduct=document.querySelector("#allProduct")
-
-// !Variables
-
-
-const macBook = 1474.99;
-const keyboard = 185.99;
-const mouse = 89.99;
-
-// * Calculators
-
-let quantityMac = Number(MacQuantity.textContent);
-let priceMAC = Number(MacTotal.textContent);
-
-let quantityKeyboard = Number(keyboardQuantity.textContent);
-let priceKeyboard = Number(keyboardTotal.textContent);
-
-let quantityApple=Number(AppleQuantity.textContent);
-let priceApple = Number(AppleTotal.textContent);
-
-// ? Events of Mac
-
-
-
-  
-  // Event listener for Mac quantity changes
-  MacbookPlusBtn.addEventListener("click", (e) => {
-    if (e.target.id == "macPlus") {
-      ++quantityMac;
-    }
-    MacQuantity.textContent = quantityMac;
-    if (MacQuantity.textContent == "0" || MacQuantity.textContent < "0") {
-      MacTotal.textContent = 0;
+ürünlerPanelDivi.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-minus")) {
+    if (e.target.nextElementSibling.innerText > 1) {
+      e.target.nextElementSibling.innerText--;
+      ürünCarpiBirimFiyat(e.target.closest(".main__product-info"));
     } else {
-      MacTotal.textContent = Number(MacQuantity.textContent) * macBook;
-    }
-    calculateTotalPrice(); // Call the function to update the total price
-    allPro()
-  });
-  
-  MacbookMinBtn.addEventListener("click", (e) => {
-    if (e.target.id == "macMinus") {
-      if (quantityMac > 0) {
-        --quantityMac;
+      if (confirm("Silmek istediğinizden emin misiniz?")) {
+        e.target.closest(".main__product").remove();
+        araToplam();
       }
     }
-    MacQuantity.textContent = quantityMac;
-    if (MacQuantity.textContent == "0" || MacQuantity.textContent < "0") {
-      MacTotal.textContent = 0;
-    } else {
-      MacTotal.textContent = Number(MacQuantity.textContent) * macBook;
+  } else if (e.target.classList.contains("fa-plus")) {
+    e.target.previousElementSibling.textContent++;
+    ürünCarpiBirimFiyat(e.target.closest(".main__product-info"));
+  } else if (e.target.classList.contains("fa-trash-can")) {
+    if (confirm("Silmek istediğinizden emin misiniz?")) {
+      // e.target.closest(".main__product").remove();
+      e.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+      araToplam();
     }
-    calculateTotalPrice(); // Call the function to update the total price
-    allPro()
+  }
+});
+
+const ürünCarpiBirimFiyat = (ürünPaneli) => {
+  // let miktar = parseFloat(document.getElementById("quantity").textContent);
+  // console.log(miktar);
+  let miktar = ürünPaneli.querySelector("#quantity").innerText;
+  console.log(miktar);
+  let fiyat = ürünPaneli.querySelector("strong").innerText;
+  // console.log(fiyat);
+  let miktarCarpiFiyat = ürünPaneli.querySelector(".main__product-line-price");
+  miktarCarpiFiyat.innerText = (miktar * fiyat).toFixed(2);
+  console.log(miktarCarpiFiyat);
+  araToplam();
+};
+
+const araToplam = () => {
+  const miktarCarpiFiyatlarListesi = document.querySelectorAll(
+    ".main__product-line-price"
+  ); //NodeList döndürür
+  let subTotal = 0;
+  miktarCarpiFiyatlarListesi.forEach((item) => {
+    subTotal += parseFloat(item.innerText);
   });
-  
-  // Event listener for keyboard quantity changes
-  keyboardPlusBtn.addEventListener("click", (e) => {
-    if (e.target.id == "keyboardPlus") {
-      ++quantityKeyboard;
-    }
-    keyboardQuantity.textContent = quantityKeyboard;
-    if (keyboardQuantity.textContent == "0" || keyboardQuantity.textContent < "0") {
-      keyboardTotal.textContent = 0;
-    } else {
-      keyboardTotal.textContent = Number(keyboardQuantity.textContent) * keyboard;
-    }
-    calculateTotalPrice(); // Call the function to update the total price
-    allPro()
-  });
-  
-  keyboardMinBtn.addEventListener("click", (e) => {
-    if (e.target.id == "keyboardMinus") {
-      if (quantityKeyboard > 0) {
-        --quantityKeyboard;
-      }
-    }
-    keyboardQuantity.textContent = quantityKeyboard;
-    if (keyboardQuantity.textContent < "0") {
-      keyboardTotal.textContent = 0;
-    } else {
-      keyboardTotal.textContent =
-        Number(keyboardQuantity.textContent) * keyboard;
-    }
-    calculateTotalPrice(); // Call the function to update the total price
-    allPro()
-  });
-  // Event listener for Apple quantity changes
-  ApplePlusBtn.addEventListener("click", (e) => {
-    if (e.target.id == "applePlus") {
-      ++quantityApple;
-    }
-    AppleQuantity.textContent = quantityApple;
-    if (AppleQuantity.textContent == "0" || AppleQuantity.textContent < "0") {
-      AppleTotal.textContent = 0;
-    } else {
-      AppleTotal.textContent = Number(AppleQuantity.textContent) * mouse;
-    }
-    calculateTotalPrice(); // Call the function to update the total price
-    allPro()
-  });
-  
-  AppleMinBtn.addEventListener("click", (e) => {
-    if (e.target.id == "appleMinus") {
-      if (quantityApple > 0) {
-        --quantityApple;
-      }
-    }
-    AppleQuantity.textContent = quantityApple;
-    if (AppleQuantity.textContent < "0") {
-      AppleTotal.textContent = 0;
-    } else {
-      AppleTotal.textContent = Number(AppleQuantity.textContent) * mouse;
-    }
-    calculateTotalPrice(); // Call the function to update the total price
-    allPro()
-  });
-  // ...
-  
-  // Function to calculate the total price
-  function calculateTotalPrice() {
-    let allPrices =
-      parseFloat(MacTotal.textContent) +
-      parseFloat(AppleTotal.textContent) +
-      parseFloat(keyboardTotal.textContent);
-    console.log(allPrices);
-    beforePrice.textContent = allPrices;
-    console.log(allPrices)
-    
+  const araToplamFiyat = document.querySelector(".main__sum-price");
+  araToplamFiyat.innerText = subTotal.toFixed(2);
 
-    
-// !Shipping section
+  const shipping = document.querySelector(".card-shipping");
+  if (araToplamFiyat.innerText > 3000 || araToplamFiyat.innerText <= 0) {
+    shipping.innerText = 0;
+  } else {
+    shipping.innerText = 25.99;
+  }
 
-    if (allPrices > 1250) {
-        shippingPriceNumber.textContent = "Free delivery";
-        shippingPrice.classList.remove("dolar");
-      } else if(allPrices > 0) {
-        shippingPriceNumber.textContent = "25.99";
-        shippingPrice.classList.add("dolar");
-      }else if(allPrices==0){
-        shippingPriceNumber.textContent = "0"
-      }
-    
-      beforePrice.textContent = allPrices;
+  const tax = document.querySelector(".card-tax");
+  tax.innerText = (araToplamFiyat.innerText * vergiOrani).toFixed(2);
 
-    //   !Tax section
-     tax.textContent=allPrices * 0.18;
+  const genelToplam = document.querySelector(".total");
+  genelToplam.innerText = (
+    parseFloat(araToplamFiyat.innerText) +
+    parseFloat(shipping.innerText) +
+    parseFloat(tax.innerText)
+  ).toFixed(2);
 
-     if(allPrices==0){
-        total.textContent="0"
-     }else{
-     total.textContent=allPrices + shippingPrice2 + allPrices * 0.18;
-     }
-}
-  
-  // ...
-  
-  // Call the function to initialize the total price
-  calculateTotalPrice();
-  
-//   !Delete section
-body.addEventListener("click", (e) => {
-
-    if(e.target.id == "macTrash"){
-    
-    document.getElementsByClassName("main__product")[0].style.display = "none";
-    if(document.getElementsByClassName("main__product")[0].style.display = "none"){
-        MacTotal.textContent="0"
-        calculateTotalPrice();
-        quantityMac="0"
-        console.log(quantityMac)
-        allPro()
-    }
-    }
-    else if(e.target.id == "keyboardTrash"){
-    document.getElementsByClassName("main__product")[1].style.display = "none";
-    if(document.getElementsByClassName("main__product")[1].style.display = "none"){
-        keyboardTotal.textContent="0"
-        calculateTotalPrice();
-        quantityKeyboard="0"
-        allPro()
-    }
-    }else if(e.target.id == "appleTrash"){
-        document.getElementsByClassName("main__product")[2].style.display = "none";
-        if(document.getElementsByClassName("main__product")[2].style.display = "none"){
-            AppleTotal.textContent="0"
-            calculateTotalPrice();
-            quantityApple="0"
-            allPro()
-        }
-    }else if(e.target.id == "allTrash"){
-        console.log(e.target.id)
-        document.getElementsByClassName("main__product")[2].style.display = "none";
-        document.getElementsByClassName("main__product")[1].style.display = "none";
-        document.getElementsByClassName("main__product")[0].style.display = "none";
-        MacTotal.textContent="0"
-        keyboardTotal.textContent="0"
-        AppleTotal.textContent="0"
-        calculateTotalPrice();
-        quantityMac="0"
-        quantityKeyboard="0"
-        quantityApple="0"
-        allPro()
-
-    }
-    })
-
-
-// Allproduct section
-console.log(allProduct.textContent) 
-let myNumber=Number((allProduct.textContent.match(/\d+/g)).join(""));
-console.log(myNumber,typeof myNumber)
-let OverAll=0
-const allPro=()=>{
-OverAll=Number(quantityMac)+Number(quantityKeyboard)+Number(quantityApple)
-    console.log(OverAll)
-    allProduct.textContent=`(${OverAll} Products)`  
-}
-allPro()
+  // if (subTotal <= 0 || subTotal >= 3000) {
+  //   kargo = 0;
+  // } else {
+  //   kargo = 25;
+  // }
+  // const vergi = subTotal * vergiOrani;
+  // console.log(vergi);
+  // const genelToplam = subTotal + vergi + kargo;
+  // console.log(genelToplam);
+};
